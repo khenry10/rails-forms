20ea3e5a1c5068878956b56e10ef571af12c243b
Jesse Shawl <jesse@jshawl.com>
2016-03-15 10:34:01 -0400

    reseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 Cat.destroy_all
 
-Cat.create(kind:"Tiger", image_url:"http://www.defenders.org/sites/default/files/styles/large/public/tiger-dirk-freder-isp.jpg")
-Cat.create(kind:"Puma", image_url:"http://images3.wikia.nocookie.net/__cb20131020135551/shipoffools/images/5/59/Puma.jpg")
-Cat.create(kind:"Kitten", image_url:"https://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX.jpeg")
+Cat.create(kind:"Tiger", image_url:"http://i.giphy.com/R9z50rPINd5ew.gif")
+Cat.create(kind:"Puma", image_url:"http://i.giphy.com/3o8doQ6wU6ipqSMvja.gif")
+Cat.create(kind:"Kitten", image_url:"http://i.giphy.com/6uMqzcbWRhoT6.gif")
